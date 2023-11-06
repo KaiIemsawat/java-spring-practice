03d6eb8222c9638811b77a3061f9447d61f61e10
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-05 19:04:20 -0500

    setup new lesson 'One2Many', framework template

---
+++ p02_inventory/src/main/java/com/spring02/one2many/One2manyApplication.java
@@ -0,0 +1,13 @@
+package com.spring02.one2many;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class One2manyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(One2manyApplication.class, args);
+	}
+
+}
---
+++ p02_inventory/src/main/java/com/spring02/one2many/controllers/AppController.java
@@ -0,0 +1,7 @@
+package com.spring02.one2many.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AppController {
+}
