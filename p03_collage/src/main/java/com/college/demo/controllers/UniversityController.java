7dccb17048a1131608488cc05583e662be94516b
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-11-09 08:35:18 -0500

    added university details page

--- p03_collage/src/main/java/com/college/demo/controllers/UniversityController.java
+++ p03_collage/src/main/java/com/college/demo/controllers/UniversityController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class UniversityController {
         return "redirect:/universities";
     }
 
+//    Render all universities
     @GetMapping("/universities")
     public String r_allUniversities(Model model) {
         List<University> universities = universityService.findAllUniversities();
@@ -29,12 +31,21 @@ public class UniversityController {
         return "universities/universities";
     }
 
+//    Render a university
+    @GetMapping("/universities/{university_id}")
+    public String r_viewUniversity(Model model, @PathVariable("university_id") Long university_id) {
+        model.addAttribute(universityService.findUniversityById(university_id));
+        return "universities/university";
+    }
+
+//    Render add university form
     @GetMapping("/universities/new")
     public String r_newUniversityForm(Model model) {
         model.addAttribute("university", new University());
         return "universities/university_form";
     }
 
+//    Save university
     @PostMapping("/universities/save")
     public String p_saveUniversity(@Valid University university, BindingResult result) {
         if (result.hasErrors()) {
