a5c86b4613d49e1fb2efe8a5376901ca52bedf9d
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-09 20:50:36 -0500

    new Java Project. Model, Repo, Service, exception

---
+++ p07_Spring_React/springboot-react/src/main/java/com/demo/springbootreact/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.demo.springbootreact.exception;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ p07_Spring_React/springboot-react/src/main/java/com/demo/springbootreact/service/IStudentService.java
@@ -0,0 +1,19 @@
+package com.demo.springbootreact.service;
+
+
+import com.demo.springbootreact.model.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+
+    Student addStudent(Student student);
+
+    List<Student> getStudents();
+
+    Student updateStudent(Student student, Long id);
+
+    Student getStudentById(Long id);
+
+    void deleteStudent(Long id);
+}
