9600677d8c012638e893df42c350d99db3785487
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-09 21:42:12 -0500

    Delete project and prepare for another

--- p07_Spring_React/springboot-react/src/main/java/com/demo/springbootreact/exception/StudentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.demo.springbootreact.exception;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
