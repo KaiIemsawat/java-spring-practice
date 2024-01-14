b7312cabb0fb0610e27be9ec9c53758c027e44b0
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-01-14 17:38:58 -0500

    categories list

--- p04_one2many_RE/one2many/src/main/java/com/javaspringboot/one2many/controllers/CategoryController.java
+++ p04_one2many_RE/one2many/src/main/java/com/javaspringboot/one2many/controllers/CategoryController.java
@@ -1,4 +1,25 @@
 package com.javaspringboot.one2many.controllers;
 
+import com.javaspringboot.one2many.enitiy.Category;
+import com.javaspringboot.one2many.repositories.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class CategoryController {
+
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+    @GetMapping("/categories")
+    public String listCategories(Model model) {
+        List<Category> listCategories = categoryRepo.findAll();
+
+        model.addAttribute("listCategories", listCategories);
+        return "categories/categories";
+    }
 }
